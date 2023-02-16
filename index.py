b701673462d9d7625f380e2af3fec7baf90ebadc
LeandroPOliveira <leandro_lpo2@hotmail.com>
2023-02-16 17:21:51 -0300

    template

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect
 
-class Bicicleta():
+
+class Bicicleta:
     def __init__(self, nome, foto, cor, preco):
         self.nome = nome
         self.foto = foto
         self.cor = cor
         self.preco = preco
+
 
 bike1 = Bicicleta('Epic Pro', 'static/img/branca.webp', 'Branca', 'R$ 79.000,00')
 bike2 = Bicicleta('Epic S-Works', 'static/img/works.webp', 'Verde', 'R$ 114.900,00')
@@ -14,15 +16,17 @@ lista = [bike1, bike2]
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def dados():
-    return render_template('index.html', bikes=lista)
+    return render_template('index.html', bikes=lista, titulo='Coroa36 Bike Store')
+
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html')
+    return render_template('novo.html', titulo='Nova Bike')
 
-@app.route('/criar', methods=['POST',])
+
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     imagem = 'static/img/' + request.form['imagem']
@@ -31,6 +35,7 @@ def criar():
     nova_bike = Bicicleta(nome, imagem, cor, preco)
     print(imagem)
     lista.append(nova_bike)
-    return redirect('/home')
+    return redirect('/')
+
 
 app.run(debug=True)
