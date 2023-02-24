da8ad706cf83410cb98504b64aec1eed7c8c5484
LeandroPOliveira <leandro_lpo2@hotmail.com>
2023-02-24 17:16:06 -0300

    redirecionamentos

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 
 class Bicicleta:
@@ -14,15 +14,17 @@ bike2 = Bicicleta('Epic S-Works', 'static/img/works.webp', 'Verde', 'R$ 114.900,
 lista = [bike1, bike2]
 
 app = Flask(__name__)
-
+app.secret_key = 'botina'
 
 @app.route('/')
-def dados():
-    return render_template('index.html', bikes=lista, titulo='Coroa36 Bike Store')
+def index():
+    return render_template(url_for('index'), bikes=lista, titulo='Coroa36 Bike Store')
 
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Nova Bike')
 
 
@@ -37,16 +39,30 @@ def criar():
     lista.append(nova_bike)
     return redirect('/')
 
+
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
     if 'tatupeba' == request.form['senha']:
-        return redirect('/')
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + 'logado com sucesso')
+        proxima_pagina = request.form['proxima']
+        return redirect(f'/{proxima_pagina}')
     else:
+        flash('Usuario não encontrado')
         return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso')
+    return redirect('/')
 
 
 app.run(debug=True)
