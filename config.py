04b7a7d087056300a90542faf114b6771c60628b
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-05-03 22:04:29 -0300

    teste postgres

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ import os
 
 SECRET_KEY = 'botina'
 
-SQLALCHEMY_DATABASE_URI = \
-'{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-SGBD= 'mysql+mysqlconnector',
-servidor = 'localhost',
-usuario = 'root',
-senha = os.environ['senha'],
-database = 'catalogo')
+# postgres://bikedata_user:y8Ra0f445ZjEREBE5hrEWyBMGZ5m0U8d@dpg-ch8rb6rhp8u0vhfplnk0-a.oregon-postgres.render.com/bikedata
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+# SGBD= 'mysql+mysqlconnector',
+# servidor = 'localhost',
+# usuario = 'root',
+# senha = os.environ['senha'],
+# database = 'catalogo')
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
