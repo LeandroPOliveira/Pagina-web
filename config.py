2e7c371606710f0ab5829bad63a82065ff88af35
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-05-03 22:12:43 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 SECRET_KEY = 'botina'
 
-# postgres://bikedata_user:y8Ra0f445ZjEREBE5hrEWyBMGZ5m0U8d@dpg-ch8rb6rhp8u0vhfplnk0-a.oregon-postgres.render.com/bikedata
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
 # SGBD= 'mysql+mysqlconnector',
