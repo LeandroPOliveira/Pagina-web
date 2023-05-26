335d17176c3f8b6b46ebeea29481028d5522b768
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-05-25 22:15:13 -0300

    marcas carousel

--- static/script.js
+++ static/script.js
@@ -1,54 +1,10 @@
-// slider
-const slides = document.getElementsByClassName("slides"); // this selection is a live collection; any changes in DOM is updated in the variable unlike querySelectors
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
 
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
-let currentSlideIndex = 0;
-let lastSlideIndex = slides.length - 1;
-
-// go to a slide;
-function goToSlide(slideIndex) {
-    [...slides].forEach((s, i) => {
-        s.style.transform = `translateX(${100 * (i - slideIndex)}%)`
-    })
-    currentSlideIndex = slideIndex;
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
-goToSlide(currentSlideIndex);
-
-// make ready the next slide if current slide is the first or the last slide
-function readyNextSlide() {
-    // if currentSlide is the last slide, shift the first slide to the end
-    if (currentSlideIndex === lastSlideIndex) {
-        slides[lastSlideIndex].insertAdjacentElement("afterend", slides[0]);
-        slides[lastSlideIndex].style.transform = `translateX(${100}%)`;
-        currentSlideIndex--; //this is because current slide is now the second last slide
-    }
-    // if currentSlide is the first slide, shift the last slide to the beginning
-    if (currentSlideIndex === 0) {
-        slides[0].insertAdjacentElement("beforebegin", slides[lastSlideIndex]);
-        slides[0].style.transform = `translateX(-${100}%)`;
-        currentSlideIndex++; //this is because current slide is now the second slide
-    }
-}
-
-// put the last slide in the beginning; ('if' condition is not necessary but providing if condition is future proof if user sets the initial slide to be shown as the last slide )
-if (currentSlideIndex === lastSlideIndex || currentSlideIndex === 0) readyNextSlide();
-
-// shift all slides left or right based on direction provided
-function shiftSlides(direction) {
-    direction ? currentSlideIndex++ : currentSlideIndex--
-    if (currentSlideIndex === lastSlideIndex || currentSlideIndex === 0) readyNextSlide();
-    goToSlide(currentSlideIndex);
-}
-tempo = 3000;
-
-function start() {
-    setInterval(() => {
-        shiftSlides(null, 1)
-    }, tempo)
-}
-
-window.addEventListener('load', start)
-
-
-
 
