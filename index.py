314c25fe3ee40c8f610b5722134cf45d70a77048
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-05-31 21:19:48 -0300

    ano e descricao

--- index.py
+++ index.py
@@ -14,4 +14,4 @@ from views_bike import *
 from views_user import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from index import db
 
+
 class Bikes(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(50), nullable=False)
     cor = db.Column(db.String(40), nullable=False)
     preco = db.Column(db.Numeric(20, 2), nullable=False)
+    ano = db.Column(db.String(40), nullable=False)
+    descricao = db.Column(db.String(40), nullable=False)
 
     def __repr__(self):
         return '<Name %r>' % self.nome
