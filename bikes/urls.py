1e9a62d8d0a2549b117ae3461af4e29724385897
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-08-14 22:35:33 -0300

    ajuste alertas e css

--- bikes/urls.py
+++ bikes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from bikes.views import index, nova_bike, editar_bike
+from bikes.views import index, nova_bike, editar_bike, filtro
 
 urlpatterns = [
     path('', index, name='index'),
     path('nova-bike', nova_bike, name='nova_bike'),
-    path('editar-bike', editar_bike, name='editar_bike')
+    path('editar-bike', editar_bike, name='editar_bike'),
+    path('filtro/<str:categoria>', filtro, name='filtro')
 ]
--- bikes/views.py
+++ bikes/views.py
@@ -19,17 +19,22 @@ def nova_bike(request):
         return redirect('login')
 
     form = BikesForms()
-    for field in form:
-        print(field.name)
     if request.method == 'POST':
         form = BikesForms(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, 'Nova bicicleta cadastrada!')
             return redirect('index')
+        else:
+            print(form.errors.as_data())
 
     return render(request, 'bikes/nova_bike.html', {'form': form})
 
 
 def editar_bike(request):
     return render(request, 'bikes/editar_bike.html')
+
+
+def filtro(request, categoria):
+    bikes = Bikes.objects.order_by('data_fotografia').filter(publicada=True, categoria=categoria)
+    return render(request, 'bikes/index.html', {'cards': bikes})
